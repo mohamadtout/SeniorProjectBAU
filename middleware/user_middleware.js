3748f64fc03c31a6884e170665b59fb9b169be40
PotatoManFelix <mohamadtout.mt@gmail.com>
2024-05-05 16:22:52 +0300

    hotfix user login

--- middleware/user_middleware.js
+++ middleware/user_middleware.js
@@ -4,9 +4,9 @@ METHODS IN THIS MIDDLEWARE:
     header and decodes it to get the userId of the currently logged in user and puts it in the body
 2.verifySessionOptional: used in the methods in the user_controller that can benefit from and utilize a login, takes the token in the Authorization
     header and decodes it to get the userId of the currently logged in user and puts it in the body
-3.verifyAgent: used in version control/support (sepcifically on mobile development) so that older versions
+4.verifyAgent: used in version control/support (sepcifically on mobile development) so that older versions
     can be brought out of support from the server side
-4.logRequest: logs the current request performed by a logged in user in the database
+5.logRequest: logs the current request performed by a logged in user in the database
     this is to identify potential "bad-faith" behavior through a future admin dashboard
 */
 
@@ -20,7 +20,7 @@ const verifySession = async (req, res, next) => {
             return res.status(401).json({ error: "Access denied" });
         } else {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            const [user] = await db.execute("SELECT u_id FROM user WHERE u_id = ?", [
+            const [user] = await db.execute("SELECT u_id FROM user WHERE u_id = ? AND user_on = 0", [
                 decoded.userId,
             ]);
             if (user.length != 0) {
@@ -43,7 +43,7 @@ const verifySessionOptional = async (req, res, next) => {
             next();
         } else {
             const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            const [user] = await db.execute("SELECT u_id FROM user WHERE u_id = ?", [
+            const [user] = await db.execute("SELECT u_id FROM user WHERE u_id = ? AND user_on = 0", [
                 decoded.userId,
             ]);
             if (user.length != 0) {
@@ -82,7 +82,7 @@ const logRequest = async (req, res, next) => {
         const client = req.header("User-Agent");
         const { userId } = req.body;
         await db.execute(
-            `INSERT INTO user_log (l_id, request, endpoint, user_id, requested_at, agent) VALUES (NULL, ?, ?, ?, CURRET_TIMESTAMP(), ?)`,
+            `INSERT INTO user_log (l_id, request, endpoint, user_id, requested_at, agent) VALUES (NULL, ?, ?, ?, CURRENT_TIMESTAMP(), ?)`,
             [JSON.stringify(req.body), JSON.stringify(req.originalUrl), userId, client]
         );
         next();
